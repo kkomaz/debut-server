db9f9e02f292081d7115687ace57ffb88a2449f1
Alex Lee <alex@uniteus.com>
2019-02-15 16:30:15 -0500

    Script update

--- scripts/runDappManifest.js
+++ scripts/runDappManifest.js
@@ -15,19 +15,41 @@ const migrate = async () => {
 
   for (dapp of dappsToUpdate) {
     const { data } = await axios.get(`${dapp.url}/manifest.json`)
-    const mongoDapp = await mongo.find({ radiksType: 'Dapp', url: dapp.url }).next()
-    dbDapp.update(
-      { url: mongoDapp.url },
-      {
-        name: data.name,
-        description: data.description,
-        start_url: data.start_url,
-        url: data.url || data.start_url,
-        icons: data.icons
-      },
-      { upsert: true }
-    )
+    await mongo.update({
+      "url": dapp.url
+    }, {
+      $set: {
+        "description": data.description,
+        "icons": data.icons,
+        "name": data.name,
+        "start_url": data.start_url ? data.start_url : data.url
+      }
+    }, {
+      upsert: true
+    })
   }
+
+  // const dbDapp = await mongo.find({ radiksType: 'Dapp', url: 'https://www.stealthy.imz' }).next()
+  // await mongo.update({ "url": "https://www.stealthy.im"}, {$set: {
+  //   "url": "https://www.stealthy.im",
+  //   "start_url": "whatever I want"
+  // }})
+  //
+  // for (dapp of dappsToUpdate) {
+  //   const { data } = await axios.get(`${dapp.url}/manifest.json`)
+  //   const mongoDapp = await mongo.find({ radiksType: 'Dapp', url: dapp.url }).next()
+  //   dbDapp.update(
+  //     { url: mongoDapp.url },
+  //     {
+  //       name: data.name,
+  //       description: data.description,
+  //       start_url: data.start_url,
+  //       url: data.url || data.start_url,
+  //       icons: data.icons
+  //     },
+  //     { upsert: true }
+  //   )
+  // }
 }
 
 migrate().then(() => {
