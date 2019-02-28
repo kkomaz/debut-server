f6f6f76c984255a5aeb2e671cc431e81498e418c
Alex Lee <alex@uniteus.com>
2019-02-28 15:33:59 -0500

    Update script

--- scripts/runDappManifest.js
+++ scripts/runDappManifest.js
@@ -4,30 +4,35 @@ const { mongoURI } = require('../config/keys');
 
 const migrate = async () => {
   const dappsToUpdate = []
-  const mongo = await getDB(mongoURI)
+  const mongo = await getDB('mongodb://localhost:27017/radiks-server')
   const dbDapp = await mongo.find({ radiksType: 'Dapp' })
   const dbDappToArray = await dbDapp.toArray()
 
   dbDappToArray.forEach((dapp) => {
-    if (!dapp.name && dapp.url) {
+    if (!dapp.icons && dapp.url) {
       dappsToUpdate.push(dapp)
     }
   })
 
   for (dapp of dappsToUpdate) {
-    const { data } = await axios.get(`${dapp.url}/manifest.json`)
-    await mongo.update({
-      "url": dapp.url
-    }, {
-      $set: {
-        "description": data.description,
-        "icons": data.icons,
-        "name": data.name,
-        "start_url": data.start_url ? data.start_url : data.url
-      }
-    }, {
-      upsert: true
-    })
+    try {
+      const { data } = await axios.get(`${dapp.url}/manifest.json`)
+      await mongo.update({
+        "url": dapp.url
+      }, {
+        $set: {
+          "description": data.description,
+          "icons": data.icons,
+          "name": data.name,
+          "start_url": data.start_url ? data.start_url : data.url
+        }
+      }, {
+        upsert: true
+      })
+    } catch (e) {
+      console.log(dapp)
+      console.log(e.message)
+    }
   }
 }
 
@@ -36,5 +41,5 @@ migrate().then(() => {
   process.exit()
 }).catch((error) => {
   console.error(error)
-  process.exist()
+  throw new Error(error.message)
 })
