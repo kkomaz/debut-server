430c94e1a36de0165e5b98820edce8fb4b657e25
Alex Lee <alex@uniteus.com>
2019-03-18 13:25:51 -0400

    Api endpoint setup

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const { setup } = require('radiks-server')
 const keys = require('./config/keys');
+const expressWS = require('express-ws');
+const { COLLECTION } = require('radiks-server/app/lib/constants');
+const fs = require('fs');
+const https = require('https');
 
 /**
  * Registered via single app
  * node index.js
 */
 const app = express()
+expressWS(app)
 
 setup({
   mongoDBUrl: keys.mongoURI
 }).then((RadiksController) => {
+  const db = RadiksController.DB
+  const radiksData = db.collection(COLLECTION);
+
   app.use('/radiks', RadiksController);
 
   app.get('/', (req, res) => {
     res.send({ hi: 'there '})
+  })
+
+  app.get('/users/random', async (req, res) => {
+    const users = await radiksData.aggregate([
+      { $match:  {radiksType: 'BlockstackUser'} },
+      { $sample: { size: 3 } }
+    ]).toArray()
+    res.send({ users })
   })
 });
 
